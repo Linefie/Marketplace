0bd557836acadc7266eccd7a08364841af5bb45e
Line Jensen <lysline85@hotmail.com>
2020-09-10 11:19:14 +0200

    Adding behavior kap 5

--- Marketplace.Domain/ClassifiedAd.cs
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -16,6 +16,14 @@ namespace Marketplace.Domain
             Id = id;
         }
 
+        // We have added three straightforward methods, and you might feel a bit disappointed because these are property setters (not even glorified). 
+        //But, what we do here is express the idea of using Ubiquitous Language in code,
+        //and transfer words from sticky notes (commands in this case) to methods.
+        public void SetTitle(string title) => _title = title;
+        public void UpdateText(string text) => _text = text;
+        public void UpdatePrice(decimal price) => _price = price;
+
+
         private Guid _ownerId;
 
         private string _title;
